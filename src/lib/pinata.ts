8dd235730e1dcd897f62bdb91ac407c30e470cd8
Alaa Saleh <alaa.saleh@protofire.io>
2026-01-02 15:51:01 -0300

    rename .env variable NEXT_PUBLIC_PINATA_GATEWAY_URL

--- src/lib/pinata.ts
+++ src/lib/pinata.ts
@@ -6,11 +6,13 @@ if (!process.env.PINATA_JWT) {
   throw new Error("PINATA_JWT environment variable is not set");
 }
 
-if (!process.env.NEXT_PUBLIC_GATEWAY_URL) {
-  throw new Error("NEXT_PUBLIC_GATEWAY_URL environment variable is not set");
+if (!process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL) {
+  throw new Error(
+    "NEXT_PUBLIC_PINATA_GATEWAY_URL environment variable is not set"
+  );
 }
 
 export const pinata = new PinataSDK({
   pinataJwt: process.env.PINATA_JWT,
-  pinataGateway: process.env.NEXT_PUBLIC_GATEWAY_URL,
+  pinataGateway: process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL,
 });

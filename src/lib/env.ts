cbbc2ed41974a451fdf2fdfa10fdf8f7c375d2c3
Alaa Saleh <alaa.saleh@protofire.io>
2026-01-06 15:12:23 -0300

    improve env variables validation

---
+++ src/lib/env.ts
@@ -0,0 +1,31 @@
+if (!process.env.NEXT_PUBLIC_ENVIRONMENT) {
+  throw new Error("NEXT_PUBLIC_ENVIRONMENT environment variable is not set");
+}
+
+if (!process.env.PINATA_JWT) {
+  throw new Error("PINATA_JWT environment variable is not set");
+}
+
+if (!process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL) {
+  throw new Error(
+    "NEXT_PUBLIC_PINATA_GATEWAY_URL environment variable is not set"
+  );
+}
+
+if (!process.env.NEXT_PUBLIC_WC_PROJECT_ID) {
+  throw new Error("NEXT_PUBLIC_WC_PROJECT_ID environment variable is not set");
+}
+
+if (!process.env.NEXT_PUBLIC_ALCHEMY_API_KEY) {
+  throw new Error(
+    "NEXT_PUBLIC_ALCHEMY_API_KEY environment variable is not set"
+  );
+}
+
+export const env = {
+  environment: process.env.NEXT_PUBLIC_ENVIRONMENT,
+  pinataJwt: process.env.PINATA_JWT,
+  pinataGateway: process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL,
+  wcProjectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID,
+  alchemyApiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
+};

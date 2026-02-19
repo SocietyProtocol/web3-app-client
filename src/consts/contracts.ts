26ccfb09808b8abecc0a18b9e9a5763df1de0238
Alaa Saleh <alaa.saleh@protofire.io>
2026-02-19 17:27:34 -0300

    small fix

--- src/consts/contracts.ts
+++ src/consts/contracts.ts
@@ -1,4 +1,4 @@
-import { Address } from "viem";
+import { Address, zeroAddress } from "viem";
 import { ChainVariable } from "./types";
 
 interface ContractAddresses {
@@ -9,7 +9,7 @@ interface ContractAddresses {
 export const contracts: ContractAddresses = {
   badges: {
     sepolia: "0x76Aa1B43a651acc4320a4610af896ddfe38B428a",
-    mainnet: "0x0",
+    mainnet: zeroAddress, // Placeholder, replace with actual address when available
   },
   auction: {
     sepolia: "0x96aa924EF625bd8eF4A2C4d369408d1491453Ec5",
--- src/consts/tokens.ts
+++ src/consts/tokens.ts
@@ -1,4 +1,4 @@
-import { Address } from "viem";
+import { Address, zeroAddress } from "viem";
 import { ChainVariable } from "./types";
 
 interface Tokens {
@@ -8,6 +8,6 @@ interface Tokens {
 export const tokens: Tokens = {
   spec: {
     sepolia: "0xe4a566a04aed9f938bba16d47d859a706bc4949a",
-    mainnet: "0x0",
+    mainnet: zeroAddress, // Placeholder, replace with actual address when available
   },
 };

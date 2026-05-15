9b0c85e3e05619406442469b3d5cb5ce2ec66f1f
Alaa Saleh <alaa.saleh@protofire.io>
2026-05-15 14:55:54 -0300

    update mainnet contract addresses in contracts.ts

--- src/consts/contracts.ts
+++ src/consts/contracts.ts
@@ -1,4 +1,4 @@
-import { Address, zeroAddress } from "viem";
+import { Address } from "viem";
 import { ChainVariable } from "./types";
 
 interface ContractAddresses {
@@ -12,7 +12,7 @@ interface ContractAddresses {
 export const contracts: ContractAddresses = {
   badges: {
     sepolia: "0x12406bB6d11Cb641e100F1d8595c0C5e63F0bD47",
-    mainnet: "0xa3af0da9733061da88b91ea28740780a887c8ce3",
+    mainnet: "0x2313C0cDdc233c92d16c2cfE17DF5fDCcE556763",
   },
   auction: {
     sepolia: "0x96aa924EF625bd8eF4A2C4d369408d1491453Ec5",
@@ -24,10 +24,10 @@ export const contracts: ContractAddresses = {
   },
   vipManager: {
     sepolia: "0x51B7481dDe7Fd0b793dbF0CAabcCd6B78D9790DF",
-    mainnet: "0x91715d95004Bd57eDC1E0FD718688CEd475E130A",
+    mainnet: "0x884026e36e772d72beE641F6C0d78A875e867117",
   },
   communityRegistry: {
     sepolia: "0xE517446f4AC1035E334B5AEc57941F2644BA8639",
-    mainnet: zeroAddress, // Placeholder, to be updated when deployed
+    mainnet: "0xEa008f15E1454C79D6AA7B95Dd3E1d39Ba32EB76",
   },
 };
